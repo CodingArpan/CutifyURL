e7e9e7f33edd01bc5a9387701e4323101c0fb97d
codingarpan <contact@codingarpan.tech>
2023-08-06 21:56:27 +0530

    Desktop Navbar Created Successfully

---
+++ packages/webui/src/components/Upper/Header.index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Reusable/Navbar";
+
+const HeaderIndex = (): JSX.Element => {
+  return (
+    <div className="bg-[#615efd] ">
+      <Navbar />
+      
+    </div>
+  );
+};
+
+export default HeaderIndex;
