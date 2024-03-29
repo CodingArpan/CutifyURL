9ab27b47c569349e8a3a45ec143d5f7153a3053c
codingarpan <contact.codingarpan@gmail.com>
2024-03-29 09:53:16 +0530

    trying to solve Application error: a client-side exception has occurred (see the browser console for more information).

--- packages/webui/src/components/LinkManager/Detailsview/LinkAnalytics/LinkLists.tsx
+++ packages/webui/src/components/LinkManager/Detailsview/LinkAnalytics/LinkLists.tsx
@@ -43,7 +43,7 @@ const LinkLists = (props: LinkListProps) => {
         console.log(result, "--------------------------");
       };
 
-      getallurldata();
+      // getallurldata();
     } catch (err) {
       console.log(err, "-----------------");
     }
